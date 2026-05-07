68bb7a2070804e42d215ae48e352c9195bd9965b
FelipeUCC898 <felipe.narvaezg@campusucc.edu.co>
2026-05-07 02:16:02 -0500

    Fix Render deployment: update build.sh paths, add gunicorn, fix URL and form indentation

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
-python manage.py migrate
+python calificaciones/manage.py collectstatic --no-input
+python calificaciones/manage.py migrate
